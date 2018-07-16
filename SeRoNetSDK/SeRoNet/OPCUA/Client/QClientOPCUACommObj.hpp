362e1b636005ca50da824fed31bddfd51144bb1a
Sebastian Friedl <sebastian.friedl@isw.uni-stuttgart.de>
2018-07-16 11:23:57 +0200

    adding naming Service for QueryClients

--- SeRoNetSDK/SeRoNet/OPCUA/Client/QClientOPCUACommObj.hpp
+++ SeRoNetSDK/SeRoNet/OPCUA/Client/QClientOPCUACommObj.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "QClientOPCUA.hpp"
+#include "NamingServiceOpcUa.hpp"
 #include <open62541.h>
 #include <SeRoNetSDK/SeRoNet/OPCUA/Client/AsyncAnswerFactoryWithCommObject.hpp>
 
@@ -17,7 +18,7 @@ template<class RequestType, class AnswerType>
 class QClientOPCUACommObj :
     public QClientOPCUA<RequestType, AnswerType> {
  public:
-  QClientOPCUACommObj(Smart::IComponent *pComponent, std::shared_ptr<UA_Client> pClient, std::mutex &opcuaThreadMutex);
+  QClientOPCUACommObj(Smart::IComponent *pComponent);
   ~QClientOPCUACommObj() = default;
 
   Smart::StatusCode queryRequest(
@@ -25,16 +26,22 @@ class QClientOPCUACommObj :
       typename QClientOPCUA<RequestType, AnswerType>::QueryIDtype &id
   ) override;
   Smart::StatusCode blocking(const bool blocking) override;
+  Smart::StatusCode connect(const std::string &server, const std::string &service) override;
  protected:
-  AsyncAnswerFactoryWithCommObject<RequestType, AnswerType> m_Factory;
+  std::shared_ptr<AsyncAnswerFactoryWithCommObject<RequestType, AnswerType>> m_Factory;
+  std::shared_ptr<SeRoNet::OPCUA::Client::NamingServiceOpcUa> m_namingService;
+  SeRoNet::OPCUA::Client::UaClientWithMutex_t::shpType m_pUaClientWithMutex;
+
 };
 
 template<class RequestType, class AnswerType>
 inline QClientOPCUACommObj<RequestType, AnswerType>::QClientOPCUACommObj(
-    Smart::IComponent *pComponent,
-    std::shared_ptr<UA_Client> pClient,
-    std::mutex &opcuaThreadMutex
-) : m_Factory(pClient, opcuaThreadMutex), QClientOPCUA<RequestType, AnswerType>::QClientOPCUA(pComponent) {
+    Smart::IComponent *pComponent
+) :
+    QClientOPCUA<RequestType, AnswerType>::QClientOPCUA(pComponent),
+    m_namingService(std::make_shared<SeRoNet::OPCUA::Client::NamingServiceOpcUa>()),
+    m_pUaClientWithMutex(nullptr)
+{
 }
 
 template<class RequestType, class AnswerType>
@@ -42,7 +49,8 @@ inline Smart::StatusCode QClientOPCUACommObj<RequestType, AnswerType>::queryRequ
     const RequestType &request,
     typename QClientOPCUA<RequestType, AnswerType>::QueryIDtype &id
 ) {
-  typename QClientOPCUA<RequestType, AnswerType>::QueryIDtype::element_type::shpAsyncAnswer_t ans(m_Factory.call(request));
+  typename QClientOPCUA<RequestType, AnswerType>::QueryIDtype::element_type::shpAsyncAnswer_t
+      ans(m_Factory->call(request));
   id.reset(new typename QClientOPCUA<RequestType, AnswerType>::QueryIDtype::element_type(ans));
   return ans != nullptr ? Smart::StatusCode::SMART_OK : Smart::StatusCode::SMART_ERROR;
 }
@@ -50,12 +58,22 @@ inline Smart::StatusCode QClientOPCUACommObj<RequestType, AnswerType>::queryRequ
 template<class RequestType, class AnswerType>
 inline Smart::StatusCode QClientOPCUACommObj<RequestType, AnswerType>::blocking(const bool blocking) {
   if (blocking) {
-    m_Factory.enableBlocking();
+    m_Factory->enableBlocking();
   } else {
-    m_Factory.disableBlocking();
+    m_Factory->disableBlocking();
   }
 
-  return m_Factory.isBlockingEnabled() == blocking ? Smart::StatusCode::SMART_OK : Smart::StatusCode::SMART_ERROR;
+  return m_Factory->isBlockingEnabled() == blocking ? Smart::StatusCode::SMART_OK : Smart::StatusCode::SMART_ERROR;
+}
+
+template<class RequestType, class AnswerType>
+Smart::StatusCode QClientOPCUACommObj<RequestType, AnswerType>::connect(const std::string &server,
+                                                                        const std::string &service) {
+  auto retValue = this->m_namingService->getConnectionAndNodeIdByName(server, service);
+  m_pUaClientWithMutex = retValue.connection;
+  //FIXME use new Constructor for factory with only one element SeRoNet::OPCUA::Client::UaClientWithMutex_t
+  m_Factory = std::make_shared<AsyncAnswerFactoryWithCommObject<RequestType, AnswerType>>(m_pUaClientWithMutex->pClient,m_pUaClientWithMutex->opcuaMutex);
+  return Smart::SMART_OK;
 }
 
 }
