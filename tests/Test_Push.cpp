e4a1ba7740ab1ed249a97dd9e47844c54bd4b01b
Sebastian Friedl <sebastian.friedl@isw.uni-stuttgart.de>
2018-08-29 14:17:36 +0200

    Add Tests for Push Pattern

--- tests/Test_Push.cpp
+++ tests/Test_Push.cpp
@@ -104,7 +104,7 @@ SeRoNet::CommunicationObjects::Description::IDescription::shp_t SeRoNet::Communi
   return ret;
 }
 
-TEST(Push, PushComplexDataType) {
+TEST(Push, ComplexDataTypeGetUpdate) {
 
   /// ------------ Setup Client ------------
   SeRoNet::Utils::Component compClient("Test_Push_PushPrimitive_Client");
@@ -131,7 +131,46 @@ TEST(Push, PushComplexDataType) {
   CoordinateObject serverElement = {1, 2, 3};
   ASSERT_EQ(Smart::StatusCode::SMART_OK, pushServer.put(serverElement));
   sleep(1); // startup time
-  ASSERT_EQ(Smart::StatusCode::SMART_OK, pushClient.getUpdateWait(clientElement));
+  ASSERT_EQ(Smart::StatusCode::SMART_OK, pushClient.getUpdate(clientElement));
+  ASSERT_EQ(serverElement, clientElement);
+
+  compServer.stopRunning();
+  comp_server_thread.join();
+  EXPECT_TRUE(true);
+}
+
+TEST(Push, ComplexDataTypeGetUpdateWait) {
+
+  /// ------------ Setup Client ------------
+  SeRoNet::Utils::Component compClient("Test_Push_PushPrimitive_Client");
+  SeRoNet::OPCUA::Client::PushClient<CoordinateObject> pushClient(&compClient);
+
+  /// ------------ Setup Server ------------
+  std::string serverCompName("Test_Push_ComplexDataType_Server");
+  std::string serviceName("PushPrimitive");
+  SeRoNet::Utils::Component compServer(serverCompName);
+  SeRoNet::OPCUA::Server::PushServer<CoordinateObject> pushServer(&compServer, serviceName);
+  ASSERT_EQ(SeRoNet::OPCUA::Server::OpcUaServer::instance().getServerName(), serverCompName);
+
+  std::thread comp_server_thread(&decltype(compServer)::run, &compServer);
+  sleep(1); // startup time
+  ASSERT_EQ(SeRoNet::OPCUA::Server::OpcUaServer::instance().isRunning(), true);
+
+  /// ---- connect Client -----
+  ASSERT_EQ(Smart::StatusCode::SMART_OK, pushClient.connect(serverCompName, serviceName));
+  ASSERT_EQ(Smart::StatusCode::SMART_OK, pushClient.subscribe(1));
+  /// ------------ Communicate ------------
+
+  CoordinateObject clientElement = {4, 5, 6};
+
+  CoordinateObject serverElement = {1, 2, 3};
+  Smart::StatusCode retVal = Smart::StatusCode::SMART_PASSIVE;
+  std::thread comp_client_thread([&]() { retVal = pushClient.getUpdateWait(clientElement); });
+
+  sleep(1); // startup time
+  ASSERT_EQ(Smart::StatusCode::SMART_OK, pushServer.put(serverElement));
+  comp_client_thread.join();
+  ASSERT_EQ(Smart::StatusCode::SMART_OK, retVal);
   ASSERT_EQ(serverElement, clientElement);
 
   compServer.stopRunning();
