08d83c96e01ea0dc57aadae4fc3a5e76a9c28b41
Christian von Arnim <christian.von-arnim@isw.uni-stuttgart.de>
2020-12-15 12:58:49 +0100

    Fixing some query tests

--- SeRoNetSDK/SeRoNet/OPCUA/Client/AsyncAnswerMethod.hpp
+++ SeRoNetSDK/SeRoNet/OPCUA/Client/AsyncAnswerMethod.hpp
@@ -25,54 +25,53 @@ namespace OPCUA {
 namespace Client {
 ///template<> is needed for friend definition
 template<typename T_RETURN>
-static void methodCalledCallback(UA_Client *client, void *userdata, UA_UInt32 requestId, UA_CallResponse *response);
+static void methodCalledCallback(UA_Client *pClient, void *userdata, UA_UInt32 requestId, UA_CallResponse *response);
 
 template<typename T_RETURN>
 class AsyncAnswerMethod : public AsyncAnswer<T_RETURN> {
  public:
   /// Do method call on the server
-  AsyncAnswerMethod(IBlocking::instanceStorage_t *, bool blockingEnabled, UA_Client *client,
+  AsyncAnswerMethod(IBlocking::instanceStorage_t *, bool blockingEnabled, UA_Client *pClient,
                     open62541Cpp::UA_NodeId objectId, open62541Cpp::UA_NodeId methodId,
                     const open62541Cpp::UA_ArrayOfVariant &inputs);
 
   virtual void processAnswer(UA_StatusCode result, open62541Cpp::UA_ArrayOfVariant *outputs) = 0;
 
-  ~AsyncAnswerMethod() override = default;
+  ~AsyncAnswerMethod() override;
  protected:
   UA_UInt32 m_requestId = 0;
+  UA_Client *m_pClient = nullptr;
  private:
-  friend void methodCalledCallback<T_RETURN>(UA_Client *client,
+  friend void methodCalledCallback<T_RETURN>(UA_Client *pClient,
                                              void *userdata,
                                              UA_UInt32 requestId,
                                              UA_CallResponse *response);
 
   /// Callback when an answer has been received
-  void methodCalled_callback(UA_Client *client, void *userdata,
+  void methodCalled_callback(UA_Client *pClient, void *userdata,
                              UA_UInt32 requestId, UA_CallResponse *callResponse);
 };
 
 /// Callback which gets called when method execution has finished
 template<typename T_RETURN>
-static void methodCalledCallback(UA_Client *client, void *userdata, UA_UInt32 requestId, UA_CallResponse *response) {
+static void methodCalledCallback(UA_Client *pClient, void *userdata, UA_UInt32 requestId, UA_CallResponse *response) {
   AsyncAnswerMethod<T_RETURN> *pAsyncAnswer = static_cast<AsyncAnswerMethod<T_RETURN> *>(userdata);
-  assert(pAsyncAnswer != nullptr);
-
   if (pAsyncAnswer != nullptr) {
-    pAsyncAnswer->methodCalled_callback(client, userdata, requestId, response);
+    pAsyncAnswer->methodCalled_callback(pClient, userdata, requestId, response);
   }
 
 }
 
 template<typename T_RETURN>
 inline AsyncAnswerMethod<T_RETURN>::AsyncAnswerMethod(
-    IBlocking::instanceStorage_t *instStorage, bool blockingEnabled, UA_Client *client,
+    IBlocking::instanceStorage_t *instStorage, bool blockingEnabled, UA_Client *pClient,
     open62541Cpp::UA_NodeId objectId, open62541Cpp::UA_NodeId methodId,
     const open62541Cpp::UA_ArrayOfVariant &inputs)
-    : AsyncAnswer<T_RETURN>(instStorage, blockingEnabled) {
+    : m_pClient(pClient), AsyncAnswer<T_RETURN>(instStorage, blockingEnabled) {
 
   UA_StatusCode retVal =
       UA_Client_call_async(
-          client,
+          pClient,
           *objectId.NodeId,
           *methodId.NodeId,
           inputs.VariantsSize,
@@ -85,10 +84,19 @@ inline AsyncAnswerMethod<T_RETURN>::AsyncAnswerMethod(
 
 }
 
+template<typename T_RETURN>
+inline AsyncAnswerMethod<T_RETURN>::~AsyncAnswerMethod() {
+  if(!this->hasAnswer())
+  {
+    // Make sure, that the callback is not called anymore.
+    UA_Client_modifyAsyncCallback(m_pClient, m_requestId, nullptr, nullptr);
+  }
+}
+
 ////@todo creat an exception hanlding
 template<typename T_RETURN>
 inline void AsyncAnswerMethod<T_RETURN>::methodCalled_callback(
-    UA_Client * /*client*/,
+    UA_Client * /*pClient*/,
     void * /*userdata*/,
     UA_UInt32 /*requestId*/,
     UA_CallResponse *callResponse
@@ -120,7 +128,7 @@ inline void AsyncAnswerMethod<T_RETURN>::methodCalled_callback(
 // this not available as answer might be deleted already
 template<>
 inline void AsyncAnswerMethod<void *>::methodCalled_callback(
-    UA_Client *client,
+    UA_Client *pClient,
     void *userdata,
     UA_UInt32 requestId,
     UA_CallResponse *callResponse
